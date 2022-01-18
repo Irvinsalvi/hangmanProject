7865f692f4f9f5347774c39a1921dfda158aba36
Irvinsalvi <element025@hotmail.com>
2022-01-18 15:37:19 -0700

    Added functionality to get user input

--- src/App.java
+++ src/App.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception 
     {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to Hangman!");
+        System.out.print("\nEnter a word to guess: ");
+
+        String wordToGuess = input.nextLine();
     }
 }
