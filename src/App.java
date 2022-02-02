8986e13a7d4ac5e618872187876d79a58b635106
Irvinsalvi <element025@hotmail.com>
2022-02-01 20:40:05 -0700

    Updated app.java
    Added a space

--- src/App.java
+++ src/App.java
@@ -9,6 +9,7 @@ public class App {
     public static void main(String[] args) throws Exception 
     {
         Scanner input = new Scanner(System.in);
+        
 
         System.out.println("\nWelcome to Hangman!");
         System.out.print("\nEnter a word to guess: ");
